27ae8ec9a2b12e9f55b52182ac8c057e85796e55
David Santistevan <dsantistevana99@gmail.com>
2021-04-13 15:21:40 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function mostrarSeccion(nombreSeccion,idButton){
+    document.getElementById("elementos-"+nombreSeccion).style.visibility="visible";
+    document.getElementById("elementos-"+nombreSeccion).style.maxHeight=10000;
+    document.getElementById(idButton).onclick=function(){ocultarSeccion(nombreSeccion,idButton);};
+    document.getElementById(idButton).innerHTML="Ocultar";
+}
+
+function ocultarSeccion(nombreSeccion,idButton){
+    document.getElementById("elementos-"+nombreSeccion).style.visibility="hidden";
+    
+    document.getElementById(idButton).onclick=function(){mostrarSeccion(nombreSeccion,idButton);};
+    document.getElementById(idButton).innerHTML="Mostrar";
+    document.getElementById("elementos-"+nombreSeccion).style.maxHeight=15;
+}
+function fecha(){
+    var date= new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if(month < 10){
+          document.getElementById("fecha").innerHTML =`${day}/0${month}/${year}`;
+    }else{
+        document.getElementById("fecha").innerHTML =`${day}/${month}/${year}`;
+    }
+    
+}
