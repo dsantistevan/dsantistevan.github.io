1deb86ca20d16da1c55cc7c9d1f513c3c022dd91
dsantistevan <dsantistevana99@gmail.com>
2022-02-04 12:29:04 -0500

    CV update 01-2022

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 function mostrarSeccion(nombreSeccion,idButton, eng){
-    document.getElementById("elementos-"+nombreSeccion).style.display="block";
-    document.getElementById("elementos-"+nombreSeccion).style.maxHeight=10000;
+    //document.getElementById("elementos-"+nombreSeccion).style.display="block";
+    document.getElementById("elementos-"+nombreSeccion).classList.remove('elementos-no');
+    //document.getElementById("elementos-"+nombreSeccion).style.maxHeight=10000;
     document.getElementById(idButton).onclick=function(){ocultarSeccion(nombreSeccion,idButton, eng);};
     document.getElementById(idButton).innerHTML= eng ? "Hide" : "Ocultar";
 }
 
 function ocultarSeccion(nombreSeccion,idButton, eng){
-    document.getElementById("elementos-"+nombreSeccion).style.display="none";
-    
+    //document.getElementById("elementos-"+nombreSeccion).style.display="none";
+    document.getElementById("elementos-"+nombreSeccion).classList.add('elementos-no')
     document.getElementById(idButton).onclick=function(){mostrarSeccion(nombreSeccion,idButton, eng);};
     document.getElementById(idButton).innerHTML=eng ? "Show" : "Mostrar";
-    document.getElementById("elementos-"+nombreSeccion).style.maxHeight=15;
+    //document.getElementById("elementos-"+nombreSeccion).style.maxHeight=15;
 }
 function fecha(){
     var date= new Date();
